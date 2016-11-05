18b1bf5e5a8a9e6be766ba1be1bafecfae4ee5b7
David Souza dos Santos <david.xvc93@gmail.com>
2016-11-05 14:37:36 -0200

    criado xml da tela de detalhes

---
+++ app/src/main/java/br/com/fatecpg/quiz_com_gravacao/DetalhesActivity.java
@@ -0,0 +1,13 @@
+package br.com.fatecpg.quiz_com_gravacao;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetalhesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalhes);
+    }
+}
